c413b11283c7d6384ce7d0d5f3ad25df4199f152
m4ttr4ymond <m4tt.r4ymond@gmail.com>
2019-09-19 14:34:56 -0700

    exported py file

--- Assignment 2.py
+++ Assignment 2.py
@@ -2,21 +2,21 @@
 # coding: utf-8
 
 # # Write-Up
-# 
+#
 # 1. Describe the problem (4 points) :
 #     1. Describe the dataset
 #         - This dataset comes from the 2016 US Census. I've taken a subset of a dataset concerning "Means of Transportation" in order to simplify my analysis, and am using data on "income related to national poverty level" and "method of transportation".
-#         
+#
 #     2. What the features represent
 #         - Each row is a zip code, and it contains the number of people for that zip code that fit into each of the gien categories.
 #         - Each column is a category that people can fit into. There are 6 main categories: driving alone, carpooling, public transportation, walking, unvconventional means, and working from home. These are then split into three lower levels: less than 100% of the poverty level, 100-149% of the poverty level and at least 150% of the poverty level.
-#         
+#
 #     3. What the target variable is (trying to predict)
 #         - Through this analysis, I'm hoping that I will be able to predict the percentage of a zip code that lives below the poverty line based on the popularity of different modes of transportation.
-#         
+#
 # 2. Split into train and test sets (2 point)
 #     - 70% for training, 30% for testing
-# 
+#
 # 3. Implement KNN from scratch (8 points)
 #     1. Include plots showing quality metrics for varying numbers of k
 #         - Included below
@@ -24,14 +24,14 @@
 #         - My recomended value is $k=7$
 #     3. Why did you choose this value?
 #         - because that is the value where the KNN is most accurate
-# 
+#
 # 4. Train a model using Scikit learn KNN (6 points)
 #     1. Compare the results to part 1
 #     2. Include plots showing quality metrics for varying numbers of k
 #     3. Recommend a value for k
-# 
+#
 # 4. Why did you choose this value?
-# 
+#
 # 5. Extra credit
 #     1. Perform cross-validation (1 point)
 #     2. Visualize decision boundary in two dimensions (if your data is more than 2d only use 2 features) (1 point)
@@ -54,9 +54,9 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 # General overview of the data
-names = ["numTotal", "bottomTotal", "midTotal", "topTotal", "numCTVAlone", "bottomCTVAlone", "midCTVAlone", 
-         "topCTVAlone", "numCTVPool", "bottomCTVPool", "midCTVPool", "topCTVPool", "numPublic", "bottomPublic", 
-         "midPublic", "topPublic", "numWalked", "bottomWalked", "midWalked", "topWalked", "numTMBO", 
+names = ["numTotal", "bottomTotal", "midTotal", "topTotal", "numCTVAlone", "bottomCTVAlone", "midCTVAlone",
+         "topCTVAlone", "numCTVPool", "bottomCTVPool", "midCTVPool", "topCTVPool", "numPublic", "bottomPublic",
+         "midPublic", "topPublic", "numWalked", "bottomWalked", "midWalked", "topWalked", "numTMBO",
          "bottomTMBO", "midTMBO", "topTMBO", "numHome", "bottomHome", "midHome", "topHome", "pUnderP"]
 data = pd.read_csv("Working_Data_Raw_Training.csv", names=names)
 testingData = pd.read_csv("Working_Data_Raw_Testing.csv", names=names)
@@ -162,7 +162,7 @@ response = getResponse(neighbors)
 def runKNNOnSingle(source, test, k):
     neighbors = getNeighbors(source, test, k)
     return getResponse(neighbors)
-        
+
 
 
 # In[146]:
@@ -195,7 +195,7 @@ def runTest(trainingSet, testingSet, k):
     correct = 0
     for i in range(len(testingSet)):
         correct += testData(trainingSet, testingSet.loc[i], k)
-    
+
     return correct/total
 
 
@@ -206,6 +206,7 @@ numOfKs = 20
 
 k_values = [None]*numOfKs
 
+# This is going to take forever, so you might want to change this to just k=1
 for i in range(1, numOfKs+1):
     k_values[i] = runTest(data_perc, data_perc_testing, i)
     print("Completed k=" + str(i))
@@ -217,7 +218,7 @@ for i in range(1, numOfKs+1):
 k_x = []
 for i in range(numOfKs):
     k_x.append(i)
-    
+
 k_y = [i * 100 for i in k_values]
 
 k_fig = plt.figure()
@@ -274,8 +275,6 @@ for i in range(len(testingPredicted)):
 
 print("Accuracy =", (testingCorrectlyGuessed/len(testingPredicted)))
 
-# This is fucking stupid
-
 
 # In[191]:
 
@@ -294,7 +293,7 @@ for i in range(1, numOfKs+1):
     for j in range(len(k_test_trainingPredicted)):
         if(int(training_data_prebuilt_tags.iloc[j]) == k_test_trainingPredicted[j]):
             k_test_trainingCorrectlyGuessed+=1
-    
+
     prebuiltResults[i-1] = (k_test_trainingCorrectlyGuessed/len(training_data_prebuilt_values))
     print("Completed k=" + str(i))
 
@@ -311,7 +310,7 @@ print(prebuiltResults)
 k_prebuilt_x = []
 for i in range(numOfKs):
     k_prebuilt_x.append(i)
-    
+
 k_prebuilt_y = [i * 100 for i in prebuiltResults]
 
 k_prebuilt_fig = plt.figure()
@@ -326,7 +325,3 @@ plt.title("Percent Accuracy as a Function of K");
 
 
 # In[ ]:
-
-
-
-
